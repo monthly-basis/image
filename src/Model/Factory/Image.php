175e8890b2105dd68dd319859b2e0a271ac0bd4c
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-21 08:04:47 -0800

    Only set width and height if they exist

--- src/Model/Factory/Image.php
+++ src/Model/Factory/Image.php
@@ -15,9 +15,15 @@ class Image
     {
         $imageEntity = new ImageEntity();
 
-        $imageEntity->setUrl($array['url'])
-                    ->setWidth($array['width'])
-                    ->setHeight($array['height']);
+        $imageEntity->setUrl($array['url']);
+
+        if (!empty($array['width'])) {
+            $imageEntity->setWidth($array['width']);
+        }
+
+        if (!empty($array['height'])) {
+            $imageEntity->setHeight($array['height']);
+        }
 
         return $imageEntity;
     }
