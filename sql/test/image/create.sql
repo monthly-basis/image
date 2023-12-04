d35e68e3831477c5f4f39b08b695bf6e9ca2df32
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-12-04 16:30:34 +0000

    Add `prompt` and `revised_prompt` columns

--- sql/test/image/create.sql
+++ sql/test/image/create.sql
@@ -1,5 +1,7 @@
 CREATE TABLE `image` (
     `image_id` int(10) unsigned auto_increment,
+    `prompt` varchar(2047) default null,
+    `revised_prompt` varchar(2047) default null,
     `root_relative_url` varchar(255) default null,
     PRIMARY KEY (`image_id`),
     UNIQUE KEY `root_relative_url` (`root_relative_url`)
