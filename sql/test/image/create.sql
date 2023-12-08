e67e5410e799c1ac9f4fd28d2cb36fb4f549eb9c
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-12-08 04:28:03 +0000

    Add `image`.`directory` column

--- sql/test/image/create.sql
+++ sql/test/image/create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `image` (
-    `image_id` int(10) unsigned auto_increment,
-    `prompt` varchar(2047) default null,
-    `revised_prompt` varchar(2047) default null,
-    `root_relative_url` varchar(255) default null,
+    `image_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `prompt` varchar(2047) DEFAULT NULL,
+    `revised_prompt` varchar(2047) DEFAULT NULL,
+    `directory` varchar(255) DEFAULT NULL,
+    `root_relative_url` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`image_id`),
     UNIQUE KEY `root_relative_url` (`root_relative_url`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
